dc26561fa91518a3290baa7759177a68f6587b80
Dan Finnie <dan@danfinnie.com>
2014-02-25 23:44:45 -0500

    Add a 'use an example' button

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ $("#go").on("click", function(ev) {
   $("#test").load("/testify", { "code" : code });
   ev.preventDefault();
 });
+
+$("#use-example").on("click", function(ev) {
+  $("#code").val("class Bunker < ActiveRecord::Base \n\
+  validate :password, secure: true \n\
+  validate :location, :protected => true \n\
+  \n\
+  def alert_staff \n\
+    staph.all_alert \n\
+    Staff.alert_all \n\
+  end \n\
+  \n\
+  def alert_president \n\
+    SecretService.stand_down \n\
+    alert_staff || go_crazy \n\
+  end \n\
+end");
+  $("#go").click();
+  ev.preventDefault();
+});
